5d3e5285440200180c5b1dc4d2081715ca3ec47a
Gaurav Paruthi <gparuthi@umich.edu>
2020-12-28 10:03:29 -0800

    add apollo

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,7 +1,9 @@
+import { ApolloProvider } from '@apollo/client'
+import { client } from '../lib/apolloClient'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <ApolloProvider client={client}><Component {...pageProps} /></ApolloProvider>
 }
 
 export default MyApp
