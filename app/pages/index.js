f3992983a197e9ca56f5b9b357eb3274dd12d15d
Gaurav Paruthi <gparuthi@umich.edu>
2020-12-29 08:22:04 -0800

    Add mutation query reload hook

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { client } from '../lib/apolloClient';
-import { gql, useQuery } from '@apollo/client';
+import { gql, useMutation, useQuery } from '@apollo/client';
+import { useRouter } from "next/router"
 
 const Query = gql`
 query Todos {
@@ -11,22 +12,77 @@ query Todos {
   }
 }
 `
-client
-  .query({
-    query: Query
-  })
-  .then(result => console.log(result.data.todos));
+const AddTodoQuery = gql`
+mutation AddTodo($value: String = "") {
+  insert_todos_one(object: {value: $value}) {
+    id
+    value
+  }
+}`
+
+const DelTodoQuery = gql`
+mutation DelTodo($id: uuid!) {
+  delete_todos_by_pk(id: $id) {
+    id
+    value
+  }
+}
+`
+
 
 export default function Home() {
-  const { loading, error, data } = useQuery(Query);
+  const router = useRouter()
+  const { loading, error, data, refetch } = useQuery(Query);
+  const [updateTodo] = useMutation(AddTodoQuery, { onCompleted: ()=>refetch() });
+  const [delTodo] = useMutation(DelTodoQuery, { onCompleted: ()=>refetch() });
+
+
+  let todoInput
+
+  const onAdd = (e) => {
+    e.preventDefault()
+    console.log(todoInput.value)
+    updateTodo({ variables: { value: todoInput.value } })
+    todoInput.value = ""
+  }
+  const onDel = (id) => {
+    console.log(id)
+    delTodo({ variables: { id: id } })
+  }
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
 
   return (
     <div className={styles.container}>
-      <div>TODOS:</div>
-      {data.todos.map(t=><div>{t.value}</div>)}
+      <div className="box"></div>
+      <h1>TODOS</h1>
+      <hr />
+      {data.todos.map(t =>
+        <div key={t.id} className="todo">
+          <div>{t.value}</div>
+          <button onClick={() => onDel(t.id)}>x</button>
+        </div>)
+      }
+      <hr />
+      <input type='text' placeholder='Enter TODO'
+        ref={el => todoInput = el} /> <button onClick={onAdd}>+</button>
+      <style jsx>
+        {`
+        .todo{
+          margin: 10px;
+    border-style: dashed;
+    padding: 10px;
+    width: 200px;
+    display: flex;
+    justify-content: space-between;
+          
+        }
+          .box {
+            margin: 20px
+          }
+        `}
+      </style>
     </div>
   )
 }
