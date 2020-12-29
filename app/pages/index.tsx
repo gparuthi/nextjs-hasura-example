1f8bfc73afa627adac616d8d7f3c422e31b2606d
Gaurav Paruthi <gparuthi@umich.edu>
2020-12-29 08:35:50 -0800

    lint

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,7 +1,6 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import { gql, useMutation, useQuery } from '@apollo/client';
-import { useRouter } from "next/router"
+import { useRouter } from "next/router";
+import styles from '../styles/Home.module.css';
 
 const Query = gql`
 query Todos {
